081e217ac5ba89068607690000fd3a2ee4b88444
Aleksander Kulpin <329298036+alejandro-2026-dev@users.noreply.github.com>
2026-09-16 18:31:31 -0300

    feat: add grid, card

---
+++ src/js/data.js
@@ -0,0 +1,50 @@
+export const coffee = [
+  {
+    id: 1,
+    name: "Irish coffee",
+    description: "Fragrant black coffee with Jameson Irish whiskey and whipped milk",
+    price: 7.0,
+  },
+  {
+    id: 2,
+    name: "Kahlua coffee",
+    description: "Classic coffee with milk and Kahlua liqueur under a cap of frothed milk",
+    price: 7.0,
+  },
+  {
+    id: 3,
+    name: "Honey raf",
+    description: "Espresso with frothed milk, cream and aromatic honey",
+    price: 5.5,
+  },
+  {
+    id: 4,
+    name: "Ice cappuccino",
+    description: "Cappuccino with soft thick foam in summer version with ice",
+    price: 5.0,
+  },
+  {
+    id: 5,
+    name: "Espresso",
+    description: "Classic black coffee",
+    price: 4.5,
+  },
+  {
+    id: 6,
+    name: "Latte",
+    description: "Espresso coffee with the addition of steamed milk and dense milk foam",
+    price: 5.5,
+  },
+  {
+    id: 7,
+    name: "Latte macchiato",
+    description: "Espresso with frothed milk and chocolate",
+    price: 5.5,
+  },
+  {
+    id: 8,
+    name: "Coffee with cognac",
+    description: "Fragrant black coffee with cognac and whipped cream",
+    price: 6.5,
+  },
+];
